860c325cd3bbb3b6e214743516183b6ada14345c
João Andrade <joao.andrade.gp@gmail.com>
2026-04-20 16:14:01 -0300

    Delete update directory

--- update/pyinstallerEXE_launcher.bat
+++
@@ -1,1 +0,0 @@
-"C:\Users\Phobos\AppData\Local\Programs\Python\Python313\python.exe" -m PyInstaller launcher.py --onefile --noconsole --icon=launcher_icon.ico --hidden-import=requests --hidden-import=urllib3 --hidden-import=certifi --hidden-import=charset_normalizer --hidden-import=idna
